027149b70c7a998717f1d41899a145f18bbcb2c3
Dalton <lambertd757@gmail.com>
2023-02-20 17:37:02 -0500

    first commit

---
+++ java/src/main/java/com/walton/oscarNightGuesser/OscarNightGuesserApplication.java
@@ -0,0 +1,13 @@
+package com.walton.oscarNightGuesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscarNightGuesserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscarNightGuesserApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/walton/oscarNightGuesser/OscarNightGuesserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walton.oscarNightGuesser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscarNightGuesserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
